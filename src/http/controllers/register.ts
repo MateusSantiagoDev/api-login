ad7b9cfc455ada94ede4d80a7af5415db4b0a162
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-08-07 14:30:02 -0300

    feat: create register controller

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,26 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { MakeRegisterUseCase } from '@/use-cases/factories/make-register-use-case'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-Error'
+
+export async function register(request: FastifyRequest, replay: FastifyReply) {
+  const registerSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerSchema.parse(request.body)
+
+  try {
+    const registerUseCase = MakeRegisterUseCase()
+    registerUseCase.execute({ name, email, password })
+    return replay.status(201).send()
+  } catch (error) {
+    if (error instanceof UserAlreadyExistsError) {
+      return replay.status(409).send({ message: error.message })
+    } else {
+      throw error
+    }
+  }
+}
