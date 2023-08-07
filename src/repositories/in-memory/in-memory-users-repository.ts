8c81fbea85798db98ac8f2fc5a6e88c0e484c62a
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-08-07 10:41:56 -0300

    feat: create inMemoryUsersRepository

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,19 @@
+import { randomUUID } from 'node:crypto'
+import { Prisma, User } from '@prisma/client'
+import { UsersRepository } from '../protocols/users-repository'
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public items: User[] = []
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: randomUUID(),
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    }
+    this.items.push(user)
+    return user
+  }
+}
