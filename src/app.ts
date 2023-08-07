fa255f71b66c36bbb4fde061ad6b489e071ac33b
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-08-07 10:01:19 -0300

    feat: add fastify config

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import fastify from 'fastify'
+import { ZodError } from 'zod'
+import { env } from './env'
+
+export const app = fastify()
+
+app.setErrorHandler((error, request, replay) => {
+  if (error instanceof ZodError) {
+    return replay.status(400).send({
+      message: 'validation error.',
+      issues: error.format(),
+    })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: here-me-should-log-to-an-external-tool-like-dataDog/NewRelic/Sentry
+  }
+
+  return replay.status(500).send({ message: 'Internal server error.' })
+})
